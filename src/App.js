1e2aeac4d329a590d370461dadf73c5db85c7441
Husna Maulana <uns200345@gmail.com>
2024-02-02 19:38:42 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getMovieList, searchMovie } from "./api";
 
-function App() {
+
+const App = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result);
+    });
+  }, []); 
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return(
+        <div className='Movie-wrapper'key ={i}>
+          <div className='Movie-title'>{movie.title}</div>
+          <img className='Movie-image' src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} />
+          <div className='Movie-date'>release: {movie.release_date}</div>
+          <div className='Movie-rate'>{movie.vote_average}</div>
+        </div>
+      )
+    })
+  }
+
+  const search = async(q) => {
+    if(q.length > 3){
+      const query = await searchMovie(q)
+      setPopularMovies(query.results)
+      //console.log({query: query})
+    }
+    
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-       
+        <h1>Movie</h1>
+        <input
+          className='Movie-search'
+          placeholder='Search Movie'
+          onChange={({ target }) => search(target.value)}
+        />
+        <div className='Movie-container'>
+          <PopularMovieList />
+        </div>
       </header>
     </div>
   );
